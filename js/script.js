0f2f25bbc692c5ca0bf78d6062a3cb8603f66794
emmanuelkibet81.coder <emmanuelkiet81@gmail.com>
2026-06-15 08:26:32 +0300

    Connect register and login to homepage

--- js/script.js
+++ js/script.js
@@ -165,3 +165,11 @@ function register() {
 
     window.location.href = "login.html";
 }
+
+function goToRegister() {
+    window.location.href = "register.html";
+}
+
+function goToLogin() {
+    window.location.href = "login.html";
+}
